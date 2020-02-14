cd4a445300b4879f583a8724991621dfbcbb6702
TinWinHlaing777 <tinwinhlaingmm777@gmail.com>
2020-02-14 22:10:10 +0630

    The last commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  public items: any;
 
-  constructor() {}
+
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    this.loadData();
+  }
+  loadData() {
+    let data: any;
+    data = this.http.get('https://api.myjson.com/bins/1hfe3e');
+    data.subscribe((result: any) => {
+      this.items = result;
+    });
+  }
+  Assignment() {
+    let assignment: any;
+    assignment = this.http.get('https://api.myjson.com/bins/iipw6'); 
+    assignment.subscribe((result: any) => {
+      this.items = result.filter((items: { title: string; }) => items.title === 'Assignment');
+    });
+  }
+  Tutorial() {
+    let tutorial: any;
+    tutorial = this.http.get('https://api.myjson.com/bins/iipw6');
+    tutorial.subscribe((result: any) => {
+      this.items = result.filter((items: { title: string; }) => items.title === 'Tutorial');
+    });
+  }
+
+  Quiz() {
+    let quiz: any;
+    quiz = this.http.get('https://api.myjson.com/bins/lt9ve');
+    quiz.subscribe((result: any) => {
+      this.items = result.filter((items: { title: string; }) => items.title === 'Quiz');
+    });
+  }
 
 }
